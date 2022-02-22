2873bd973db103bccc4f39ac8e1966fee498b942
asd0990087 <46321949+asd0990087@users.noreply.github.com>
2022-02-22 23:57:59 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,20 +7,22 @@ let _87 = 0
 _87 = 0
 let _1 = 1
 basic.forever(function () {
-    basic.showString("A")
     if (_87 == 1) {
+        basic.showString("A")
+        _1 += 6
         robotbit.GeekServo2KG(robotbit.Servos.S1, _1)
-        _1 += 3
+        basic.pause(50)
     } else {
         basic.clearScreen()
         robotbit.MotorStopAll()
     }
 })
 basic.forever(function () {
-    basic.showString("B")
     if (_87 == 3) {
+        basic.showString("B")
+        _1 += -6
         robotbit.GeekServo2KG(robotbit.Servos.S1, _1)
-        _1 += -3
+        basic.pause(50)
     } else {
         basic.clearScreen()
         robotbit.MotorStopAll()
